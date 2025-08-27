9de38d1a0aad6c451b9e6d1b929ad5e714652653
rflrytdzn <kuhaku.blank.rd@gmail.com>
2025-08-27 11:56:55 +0800

    style: improve login ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { LoginForm } from "./components/loginForm";
+import { GalleryVerticalEnd } from "lucide-react";
+import Illustration from "@/../public/exam.svg";
+import Image from "next/image";
+import { LoginForm } from "@/app/(auth)/login/components/loginForm";
+import Logo from "@/../public/logo.svg";
 
-const LoginPage = () => {
+export default function LoginPage() {
   return (
-    <div className="flex h-svh items-center">
-      <LoginForm />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="  text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <Image src={Logo} alt="Logo" />
+            </div>
+            QuizMaster
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <Image
+          src={Illustration}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
     </div>
   );
-};
-
-export default LoginPage;
+}
